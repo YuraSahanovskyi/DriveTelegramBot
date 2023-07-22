e2cb372c173cf83ba30da524e8071dfb2696cad8
YuraSahanovskyi <yuriisahanovskyi@gmail.com>
2023-07-22 13:16:10 +0300

    Auth encapsulation for bot

---
+++ pkg/telegram/auth.go
@@ -0,0 +1,45 @@
+package telegram
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/YuraSahanovskyi/DriveTelegramBot/pkg/database"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"golang.org/x/oauth2"
+)
+
+func (b *Bot) checkAuth(id int64) bool {
+	tokenBytes, err := b.repo.Get(database.Token, id)
+	if err != nil {
+		return false
+	}
+	var token oauth2.Token
+	if err := json.Unmarshal(tokenBytes, &token); err != nil {
+		log.Printf("Error unmarshalling token: %v", err)
+		return false
+	}
+	if !token.Valid() {
+		return false
+	}
+	return true
+}
+
+func (b *Bot) getAuth(id int64) *oauth2.Token {
+	//get token from database
+	tokenBytes, err := b.repo.Get(database.Token, id)
+	if err != nil {
+		msg := tgbotapi.NewMessage(id, "cannot fin your token in database, try to auth again")
+		b.tg.Send(msg)
+		return nil
+	}
+	//unmarshal token
+	var token oauth2.Token
+	if err := json.Unmarshal(tokenBytes, &token); err != nil {
+		log.Printf("Error unmarshalling token: %v", err)
+		msg := tgbotapi.NewMessage(id, "cannot fin your token in database, try to auth again")
+		b.tg.Send(msg)
+		return nil
+	}
+	return &token
+}
